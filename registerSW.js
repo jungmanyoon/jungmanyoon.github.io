8d6e1ec5fc89282875995477a922935e18a6ad95
jungmanyoon <jmyoon0702@gmail.com>
2025-12-21 12:35:40 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/baking-converter/sw.js', { scope: '/baking-converter/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
